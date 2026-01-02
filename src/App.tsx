8f39aae95e0fa4715259f9daebeab7bdbab3d384
rg-a-trial <romaric.gattin@accesstrial.care>
2026-01-02 16:03:15 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 export default function App() {
-  return <h1>PROD OK</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>HOME OK</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
