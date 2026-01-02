df1f75da6b69f8ee93ae669f46c8ed22f0fae9d0
rg-a-trial <romaric.gattin@accesstrial.care>
2026-01-02 15:57:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { fileURLToPath, URL } from "node:url";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
